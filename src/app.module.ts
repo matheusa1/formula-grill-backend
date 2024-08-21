45b050828d8bc9ec01e516d10bcc2cc0a254c30e
Guilherme Inoe Araujo <119065242+GuilhermeInoe@users.noreply.github.com>
2024-08-21 19:10:47 -0300

    fix:🐛 Middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from './database/prisma.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
+
+providers: [
+  {
+    provide: 'APP_GUARD',
+    useClass: JwtAuthGuard,
+  },
+]
 
 @Module({
   imports: [UserModule, AuthModule],
   controllers: [],
-  providers: [PrismaService],
+  providers: [],
 })
 export class AppModule {}
