d49e379e1e00d5247fee61fd0fead6639b52e040
Victórya <72286081+turner404@users.noreply.github.com>
2024-08-26 21:19:12 -0300

    Update create-mesa.dto.ts
    Arrumando o bug

--- src/mesas/dto/create-mesa.dto.ts
+++ src/mesas/dto/create-mesa.dto.ts
@@ -1,8 +1,11 @@
-import { IsNotEmpty, IsInt, IsBoolean, Min } from 'class-validator';
+import { IsNotEmpty, IsInt, IsBoolean, Min, IsString } from 'class-validator';
 
 export class CreateMesaDto {
   @IsNotEmpty({
     message: 'Código da mesa é obrigatório',
+  })
+  @IsString({
+    message: 'O código da mesa deve ser uma string', //arrumando bug solicitado pelo matheus
   })
   code: string;
 
