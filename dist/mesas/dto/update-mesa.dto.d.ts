10b2675908c522dc14da1c6b9a7eddf9a478ef18
Victórya <72286081+turner404@users.noreply.github.com>
2024-08-28 18:02:42 -0300

    atualizando

---
+++ dist/mesas/dto/update-mesa.dto.d.ts
@@ -0,0 +1,5 @@
+import { CreateMesaDto } from './create-mesa.dto';
+declare const UpdateMesaDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateMesaDto>>;
+export declare class UpdateMesaDto extends UpdateMesaDto_base {
+}
+export {};
--- dist/mesas/mesas.module.js
+++ dist/mesas/mesas.module.js
@@ -6,25 +6,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppModule = void 0;
+exports.MesasModule = void 0;
 const common_1 = require("@nestjs/common");
-const user_module_1 = require("./user/user.module");
-const auth_module_1 = require("./auth/auth.module");
-const mesas_module_1 = require("./mesas/mesas.module");
-const jwt_auth_guard_1 = require("./auth/guards/jwt-auth.guard");
-let AppModule = class AppModule {
+const mesas_service_1 = require("./mesas.service");
+const mesas_controller_1 = require("./mesas.controller");
+const prisma_service_1 = require("../database/prisma.service");
+let MesasModule = class MesasModule {
 };
-exports.AppModule = AppModule;
-exports.AppModule = AppModule = __decorate([
+exports.MesasModule = MesasModule;
+exports.MesasModule = MesasModule = __decorate([
     (0, common_1.Module)({
-        imports: [user_module_1.UserModule, auth_module_1.AuthModule, mesas_module_1.MesasModule],
-        controllers: [],
-        providers: [
-            {
-                provide: 'APP_GUARD',
-                useClass: jwt_auth_guard_1.JwtAuthGuard,
-            },
-        ],
+        controllers: [mesas_controller_1.MesasController],
+        providers: [mesas_service_1.MesasService, prisma_service_1.PrismaService],
     })
-], AppModule);
-//# sourceMappingURL=app.module.js.map
+], MesasModule);
+//# sourceMappingURL=mesas.module.js.map
